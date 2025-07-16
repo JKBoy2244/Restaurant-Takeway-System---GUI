7eac0f5c2df6bc8391c20f458d206a91ee72b5d2
JKBoy2244 <jobayer2006@gmail.com>
2025-07-16 21:25:45 +0100

    Create Confirmation.java

---
+++ Confirmation.java
@@ -0,0 +1,6 @@
+class Confirmation implements ConfirmInterface {
+    @Override
+    public void confirm(UserDetails User) {
+        JOptionPane.showMessageDialog(null, "Thank you for ordering from our restaurant, " + User.getFirstName() + " " + User.getLastName() + ". Your order has been confirmed and it will be delivered to you shortly. Have a great day!");
+    }
+}
