c937139a14dca49e3426d4aca2630315b1d4b513
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 19:19:44 +0000

    Update Confirmation.java

--- Classes/Confirmation.java
+++ Classes/Confirmation.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-class Confirmation implements ConfirmInterface {
+public class Confirmation implements ConfirmInterface {
     @Override
     public void confirm(UserDetails User) {
         JOptionPane.showMessageDialog(null, "Thank you for ordering from our restaurant, " + User.getFirstName() + " " + User.getLastName() + ". Your order has been confirmed and it will be delivered to you shortly. Have a great day!");
