86ec8cdf50a94d2965dbae619e0c6134b30651d2
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 18:09:21 +0000

    Update OrderMealTest.java

--- Tests/OrderMealTest.java
+++ Tests/OrderMealTest.java
@@ -1,1 +1,20 @@
+import javax.swing.JOptionPane;
 
+public class OrderMealTest {
+
+  public static void main(String[] arg) {
+
+
+    OrderMeal order = new OrderMeal("Bob", "Daniel", "bobdaniel@yahoo.com", "09876543212", "4 PC. Wings and Chips", "Coke", "Chocolate Ice Cream", 2.50);
+    JOptionPane.showMessageDialog(order.getFirstName());
+    JOptionPane.showMessageDialog(order.getLastName());
+    JOptionPane.showMessageDialog(order.getEmailAddress());
+    JOptionPane.showMessageDialog(order.getPhoneNumber());
+    JOptionPane.showMessageDialog(order.getFirstName());
+    JOptionPane.showMessageDialog(order.getFood());
+    JOptionPane.showMessageDialog(order.getDrink());
+    JOptionPane.showMessageDialog(order.getDessert());
+    JOptionPane.showMessageDialog(order.getCost());
+  }
+  
+}
