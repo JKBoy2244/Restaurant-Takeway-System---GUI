c1088ca8be42bb6b284c045fed3d774e024a3f7c
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:44:06 +0000

    Create OrderMealTest4.java

---
+++ Tests/OrderMealTest4.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class OrderMealTest1 {
+
+  public static void main(String[] args) {
+
+
+    OrderMeal order = new OrderMeal("B", "Danielyyyyyyyyyyyyyyyyyyyyyy", "bobdaniel.com", "0987654321343343''@2", "100 Burgers", "Cake", "Chocolate Ice Cream---//@", 2.50);
+    JOptionPane.showMessageDialog(null, order.getFirstName());
+    JOptionPane.showMessageDialog(null, order.getLastName());
+    JOptionPane.showMessageDialog(null, order.getEmailAddress());
+    JOptionPane.showMessageDialog(null, order.getPhoneNumber());
+    JOptionPane.showMessageDialog(null, order.getFirstName());
+    JOptionPane.showMessageDialog(null, order.getFood());
+    JOptionPane.showMessageDialog(null, order.getDrink());
+    JOptionPane.showMessageDialog(null, order.getDessert());
+    JOptionPane.showMessageDialog(null, order.getCost());
+  }
+  
+}
